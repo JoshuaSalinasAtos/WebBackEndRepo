2eba28a36e3256b1ed31f33ae01ec649d307392b
AshleyQuezadaAtos <97918655+AshleyQuezadaAtos@users.noreply.github.com>
2022-01-26 08:26:16 -0600

    Update Topics.cs

--- src/src/Models/Topics.cs
+++ src/src/Models/Topics.cs
@@ -1,9 +1,9 @@
-﻿using System.ComponentModel;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using WebCourseRepo.Models;
 
-namespace WebBackEndRepo.Models
+
+namespace WebCourseRepo.Models
 {
     public class Topics : Entity
     {
